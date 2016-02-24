d5b4f518faf3a0fc202c20ffb63abe7d801057e7
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2016-02-24 13:41:13 +1000

    Added fzf config files

---
+++ generic/.fzf.zsh
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" =~ "~/.fzf/bin" ]]; then
+  export PATH="$PATH:~/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" =~ "~/.fzf/man" && -d "~/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:~/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "~/.fzf/shell/key-bindings.zsh"
+
